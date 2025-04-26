a9651f0167243fd5b047597642e1358a68f505c4
izalinx <izalinx67@gmail.com>
2025-04-26 20:01:54 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const dataContainer = document.getElementById('data-container');
+const apiUrl = 'https://api-e-database.kemendagri.go.id/dataset/1208?sub=tabel&page=1&kode_wilayah=&provinsi=jawa%20barat&jumlah_ormas_ber_skt=&jumlah_ormas_berbadan_hukum='; // Ganti dengan URL API yang ingin Anda gunakan
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Proses data yang diterima di sini
+    console.log('Data berhasil diterima:', data);
+    dataContainer.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`; // Menampilkan data dalam format JSON yang rapi
+  })
+  .catch(error => {
+    console.error('Gagal mengambil data:', error);
+    dataContainer.innerHTML = `<p style="color: red;">Gagal memuat data: ${error.message}</p>`;
+  });
